8441cfac241293186d16bec727b8aed3feae6980
nicolas <nicolas.mercier570@gmail.com>
2021-01-16 22:01:27 +0100

    front & back done

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { uuid } = require('uuidv4');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+let items = [
+  { id: uuid(), item: 'Learn more about React' },
+  { id: uuid(), item: 'Learn more about PWA' },
+];
+
+app.get('/items', (req, res) => {
+  res.json(items);
+});
+
+app.post('/items', (req, res) => {
+  const { todoItem } = req.body;
+  console.log(req.body);
+  items.push({ id: uuid(), item: todoItem });
+  res.json(items);
+});
+
+app.delete('/items', (req, res) => {
+  const { id } = req.body;
+  items = items.filter((item) => item.id !== id);
+  res.json(items);
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    throw new Error(err);
+  } else {
+    // eslint-disable-next-line
+    console.log(`server listening on port: ${port}`);
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [todoItem, setTodoItem] = useState('');
+
+  const fetchItems = async () => {
+    try {
+      setLoading(true);
+      const result = await axios.get('http://localhost:5000/items');
+      setItems(result.data);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const addItem = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const result = await axios.post('http://localhost:5000/items', {
+        todoItem,
+      });
+      setItems(result.data);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      setLoading(false);
+      setTodoItem('');
+    }
+  };
+
+  const deleteItem = async (id) => {
+    try {
+      setLoading(true);
+      const result = await axios.delete('http://localhost:5000/items', id);
+      setItems(result.data);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav className='navbar navbar-light bg-light'>
+        <span className='navbar navbar-brand mb-0 h1'>
+          <img src={logo} className='App-logo' alt='logo' />
+        </span>
+      </nav>
+
+      <div className='px-3 py-2'>
+        <form className='form-inline my-3' onSubmit={addItem}>
+          <div className='form-group mb-2 p-0 pr-3 col-8 col-sm-10'>
+            <input
+              className='form-control col-12'
+              placeholder='What do you need to do'
+              value={todoItem}
+              onChange={(e) => setTodoItem(e.target.value)}
+            />
+          </div>
+          <button className='btn btn-primary mb-2 col-4 col-sm-2' type='submit'>
+            Add
+          </button>
+        </form>
+
+        {loading && <p>Loading...</p>}
+
+        {!loading && items.length === 0 && (
+          <div className='alert alert-secondary'>All done :)</div>
+        )}
+
+        {!loading && items && (
+          <table className='table table-stripped'>
+            <tbody>
+              {items.map((item, i) => {
+                return (
+                  <tr key={item.id} className='row'>
+                    <td className='col-1'>{i + 1}</td>
+                    <td className='col-10'>{item.item}</td>
+                    <td className='col-1'>
+                      <button
+                        type='button'
+                        className='close'
+                        aria-label='Close'
+                        onClick={() => deleteItem(item.id)}>
+                        <span aria-hidden='true'>&times;</span>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
     </div>
   );
 }
